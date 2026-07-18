e9cfebac1d4c8d3aae6ded2a9d41747cd5164c27
Justin Hoy <justinhoyvt@gmail.com>
2026-07-18 16:22:14 -0400

    Pulse Builder: PULSE-031 replace assets/site-config.js

---
+++ assets/site-config.js
@@ -0,0 +1,9 @@
+window.PULSE_CONFIG = Object.freeze({
+  accessValidationUrl: '',
+  statusEndpointUrl: '',
+  bookingRequestUrl: '',
+  weatherEmbedUrl: '',
+  vtRoadsUrl: 'https://www.facebook.com/share/g/1DHxsDkj7Z/?mibextid=wwXIfr',
+  optionalTipUrl: ''
+});
+window.PULSE_SITE_CONFIG = window.PULSE_CONFIG;
